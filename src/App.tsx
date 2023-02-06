e77e13202ee920e0aecfeb0719dc93e304a759bc
Ahmad Yousif89 <ayob.4wrk89@gmail.com>
2023-02-06 02:13:36 +0200

    added h1

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { View } from './components/layout';
 function App() {
   return (
     <main className="main">
+      <h1 className="off-screen">Multi step form</h1>
       <View />
     </main>
   );
